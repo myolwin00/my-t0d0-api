99764aae53ff52b782ee33cdf5d17c18b046e572
Myo Lwin Oo <myolwinoo919@gmail.com>
2019-04-22 11:15:05 +0630

    changed api url

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const router = express.Router();
 const {check,validationResult} = require('express-validator/check');
 const cors = require('cors');
 const {MongoClient, ObjectId} = require('mongodb');
-const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({
+app.use(express.urlencoded({
     extended: false
 }));
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 app.use("/", router);
 
